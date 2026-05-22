6cc1731c23188b57e43c61ce26c8c37447003600
zSinani <ili18@ucsc.edu>
2026-05-22 03:08:23 -0700

    Contemporary Tutorial Scene

--- Assets/Scripts/Managers/TutorialManager.cs
+++ Assets/Scripts/Managers/TutorialManager.cs
@@ -16,21 +16,23 @@ public class TutorialManager : MonoBehaviour
     private string[] tutorialSteps =
     {
         "Welcome to BG4!\nPress [Space] to continue!", // 36
-        // Space
+        // 0- input
         "Lets start with the basics.\n Select a character with [Left Click].", // 24
-        // Character Selected
-        "Great! Now let's move around.", // 24
-        // Space/Movement input
+        // 1- Character Selected
+        "Great! Now let's try moving around.", // 24
+        // 2- input
         "[Right click] to move your selected character.\n Try navigating to the highlighted area.", // 20
-        // Reach trigger
+        // 3- Reach trigger
         "Awesome! Now let's try attacking.", // 24
-        //
+        // 4- input
         "Target an enemy by using [Right Click] on them.\n Your hero will do the rest!", //20
-        //
+        // 5- target dummy
         "Next up, abilities!", // 24
-        //
-        "Each hero has unique abilities to help them out during combat.\n Use [Q] and [W] to activate your selected hero's abilities." // 20
-        // 
+        // 6- input
+        "Each hero has unique abilities to help them out during combat.\n Use [Q] and [W] to activate your selected hero's abilities.", // 20
+        // 7- ability check, maybe check for usage of each?
+        "ABILITY_STEP_PLACEHOLDER"
+        // 8- 
     };
 
     private int currentStep = 0;
@@ -48,12 +50,22 @@ public class TutorialManager : MonoBehaviour
 
     void ProgressTutorial(int stepIndex)
     {
+        destination.SetActive(false);
         switch (stepIndex)
         {
             case 0: setTutorialText(tutorialSteps[0], 36); break;
             case 1: setTutorialText(tutorialSteps[1], 24); break;
-            case 2: setTutorialText(tutorialSteps[2], 20); break;
-            // etc
+            case 2: setTutorialText(tutorialSteps[2], 24); break;
+            case 3: setTutorialText(tutorialSteps[3], 20);
+                destination.SetActive(true);
+                break;
+            case 4: setTutorialText(tutorialSteps[4], 24); break;
+            case 5: setTutorialText(tutorialSteps[5], 20);
+                trainingDummy.gameObject.SetActive(true);
+                break;
+            case 6: setTutorialText(tutorialSteps[6], 24); break;
+            case 7: setTutorialText(tutorialSteps[7], 24); break;
+                // Ability usage checkboxes?
             default:
                 break;
         }
@@ -70,16 +82,22 @@ public class TutorialManager : MonoBehaviour
                 { NextStep(); }
                 break;
             case 2:
-                if (FetchInput())
-                {
-                    NextStep();
-                }
+                if (FetchInput()) { NextStep(); }
                 break;
             case 3:
+                if (HeroInZone()) NextStep();
                 break;
             case 4:
+                if (FetchInput()) { NextStep(); }
                 break;
             case 5:
+                if (trainingDummy.CurrentHealth < trainingDummy.MaxHealth) { NextStep(); }
+                break;
+            case 6:
+                if (FetchInput()) { NextStep(); }
+                break;
+            case 7:
+                // abiity checks
                 break;
             default:
                 break;
@@ -88,7 +106,8 @@ public class TutorialManager : MonoBehaviour
     
     private bool FetchInput()
     {
-        if (Keyboard.current != null && inpTimer >= inputDelay)
+        if ((Keyboard.current.spaceKey.isPressed || Mouse.current.rightButton.wasPressedThisFrame
+        || Mouse.current.leftButton.wasPressedThisFrame) && inpTimer >= inputDelay)
         {
             inpTimer = 0f;
             return true;
@@ -115,8 +134,9 @@ public class TutorialManager : MonoBehaviour
         tutorialText.text = text;
     }
 
-    void OnTriggerEnter(Collider other)
+    private bool HeroInZone()
     {
-        return;
+        if (destination == null || trainingHero == null) return false;
+        return Vector2.Distance(trainingHero.transform.position, destination.transform.position) < 1f;
     }
 }
