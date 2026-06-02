1f1854a95ef64e0d6e8d39cfd939ec583cb2f3a2
zSinani <ili18@ucsc.edu>
2026-06-02 03:06:41 -0700

    Add selection highlight

--- Assets/Scripts/Gameplay/CharacterSelector.cs
+++ Assets/Scripts/Gameplay/CharacterSelector.cs
@@ -1,4 +1,5 @@
 using UnityEditor;
+using UnityEditor.Callbacks;
 using UnityEngine;
 
 [RequireComponent(typeof(Collider2D))] //required for OnMouseDown to work
@@ -8,22 +9,26 @@ public class CharacterSelector : MonoBehaviour
     [Header("Selection Colors")]
     [SerializeField] private Color _selectedColor = Color.yellow;
     private SpriteRenderer _spriteRenderer;
-    private Color _baseTint; // stores the hero's tint so we can restore it on deselect
+    private GameObject highlight;
+    // private Color _baseTint; // stores the hero's tint so we can restore it on deselect
 
     void Start()
     {
         _spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+        highlight = transform.Find("SelectionHighlight")?.gameObject;
         // Remember whatever tint the hero already has (set by Hero.Start from HeroData)
-        _baseTint = _spriteRenderer.color;
+        // _baseTint = _spriteRenderer.color;
     }
     public void Select()
     {
-        _spriteRenderer.color = _selectedColor;
+        // _spriteRenderer.color = _selectedColor;
+        highlight.SetActive(true);
         Debug.Log(gameObject.name + " selected.");
     }
     public void Deselect()
     {
-        _spriteRenderer.color = _baseTint;
+        // _spriteRenderer.color = _baseTint;
+        highlight.SetActive(false);
         Debug.Log(gameObject.name + " deselected.");
     }
 }
